afa823d9c9d22557879628faf73fd7666fe45c46
soufien <aloui19940724@gmail.com>
2026-08-18 19:44:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,24 +18,75 @@ sidebar.classList.remove("active");
 overlay.classList.remove("active");
 });
 
-const searchInput = document.getElementById("searchInput");
-
-searchInput.addEventListener("keyup", () => {
-
-const value = searchInput.value.toLowerCase();
-
-const products = document.querySelectorAll(".product");
-
-products.forEach(product => {
-
-const title = product.querySelector("h3").textContent.toLowerCase();
-
-if(title.includes(value)){
-product.style.display="block";
-}else{
-product.style.display="none";
+const products = [
+{
+name: "YouTube Premium",
+price: "12 DT",
+image: "images/youtube.jpg"
+},
+{
+name: "ChatGPT Go",
+price: "15 DT",
+image: "images/chatgpt.jpg"
+},
+{
+name: "PUBG UC 60",
+price: "4 DT",
+image: "images/pubg60.jpg"
+},
+{
+name: "PUBG UC 300",
+price: "15 DT",
+image: "images/pubg300.jpg"
+},
+{
+name: "Free Fire Diamonds",
+price: "5 DT",
+image: "images/ff.jpg"
+},
+{
+name: "Google AI Plus",
+price: "18 DT",
+image: "images/gemini.jpg"
 }
+];
+
+function renderProducts(items){
+
+const container =
+document.getElementById("productsContainer");
+
+container.innerHTML = "";
+
+items.forEach(product => {
+
+container.innerHTML += `
+<div class="product">
+<img src="${product.image}" alt="${product.name}">
+<h3>${product.name}</h3>
+<p>${product.price}</p>
+</div>
+`;
 
 });
+
+}
+
+renderProducts(products);
+
+const searchInput =
+document.getElementById("searchInput");
+
+searchInput.addEventListener("input", () => {
+
+const value =
+searchInput.value.toLowerCase();
+
+const filtered =
+products.filter(product =>
+product.name.toLowerCase().includes(value)
+);
+
+renderProducts(filtered);
 
 });
